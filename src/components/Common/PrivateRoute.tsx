09106180f4bddd21e2ad35c91308436fc48dd711
De-miles <miles998321@gmail.com>
2024-12-05 22:08:51 +0700

    setup routings with react-router-dom

---
+++ src/components/Common/PrivateRoute.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export function PrivateRoute({ children }: { children: JSX.Element }) {
+  // Check if user is logged in
+  // If yes, show route
+  // Otherwise, redirect to login page
+  const isLoggedIn = Boolean(localStorage.getItem('access_token'));
+  if (!isLoggedIn) return <Navigate to="/login" />;
+
+  return children;
+}
