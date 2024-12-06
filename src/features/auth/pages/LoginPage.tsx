d1c52eff08599c823509c6faa9293f5237d94875
De-miles <miles998321@gmail.com>
2024-12-06 17:54:54 +0700

    add Material UI & setup Login page

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,27 @@
+import { Box, Button, Container, Paper, Typography } from '@mui/material';
 import * as React from 'react';
 
-export interface LoginPageProps {}
-
-export function LoginPage(props: LoginPageProps) {
-  return <div>Login Page</div>;
+export function LoginPage() {
+  return (
+    <Container
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexFlow: 'row nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Paper elevation={3} sx={{ p: 4 }}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+        <Box sx={{ mt: 4 }}>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </Container>
+  );
 }
